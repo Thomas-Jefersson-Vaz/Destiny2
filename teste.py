b14c233d07cbff0c1c20e6bde2743df1e549e509
TJVG4M34R13 <86377436+Thomas-Jefersson-Vaz@users.noreply.github.com>
2023-08-14 14:56:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,4 +3,4 @@ manifest = 'files\manifest.json'
 with open(manifest) as file:
     data = json.load(file)
 
-print(len(data['Response']['jsonWorldContentPaths']['pt-br']))
+print(data['Response']['jsonWorldContentPaths']['pt-br'])
